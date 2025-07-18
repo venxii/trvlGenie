5190d6cbd3331b79edd6263b24f1c50a8241b566
Veda Chinta <146711981+venxii@users.noreply.github.com>
2025-07-18 12:14:04 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9cOc6vUgAQdByX7Hq4YKzngsFBnvYacU",
-  authDomain: "trvlgenie-65d83.firebaseapp.com",
-  projectId: "trvlgenie-65d83",
-  storageBucket: "trvlgenie-65d83.firebasestorage.appspot.com",
-  messagingSenderId: "798345781966",
-  appId: "1:798345781966:web:2d1c2efb53a9127cc6a69f",
-  measurementId: "G-39JDWDRJPF"
+  //Use your config details that firebase gives when the firebase project is created.
 };
 
 // Initialize Firebase
